112db49f1197a60319616a71e08e910c0ba48bf8
manu <manuelsastre@metricool.com>
2019-04-21 15:25:29 +0200

    fix to github pages

---
+++ docs/build/precache-manifest.b0a4e05b3e4985e2943abdc1fa979754.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "46643978653823ae8764",
+    "url": "./static/css/main.51cfd073.chunk.css"
+  },
+  {
+    "revision": "46643978653823ae8764",
+    "url": "./static/js/main.231ed8ed.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "2423377b245ed0246e6d",
+    "url": "./static/css/2.509dcfb9.chunk.css"
+  },
+  {
+    "revision": "2423377b245ed0246e6d",
+    "url": "./static/js/2.293d627c.chunk.js"
+  },
+  {
+    "revision": "c2b42dd0f1cc6265f650c1f3381f99a7",
+    "url": "./static/media/logo.c2b42dd0.png"
+  },
+  {
+    "revision": "7e739167f284ce5b46f3a7516996fbae",
+    "url": "./index.html"
+  }
+];
